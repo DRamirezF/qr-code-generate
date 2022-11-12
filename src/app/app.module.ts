42a4f18b7e22b8fe24f1b022f98d9d1eecf140d8
DRamirezF <danielavelino_27@hotmail.com>
2022-11-11 22:20:11 -0400

    header&footer  ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './shared/layout/header/header.component';
+import { QrGeneretorComponent } from './pages/qr-generetor/qr-generetor.component';
+import { FooterComponent } from './shared/layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    QrGeneretorComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/qr-generetor/qr-generetor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qr-generetor',
+  templateUrl: './qr-generetor.component.html',
+  styleUrls: ['./qr-generetor.component.scss']
+})
+export class QrGeneretorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
